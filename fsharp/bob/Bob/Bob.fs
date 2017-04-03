4fdc7dcd01a4d90afcd20ee02c7c90aff07a4c71
Emily Gorcenski <gorcee@hotmail.com>
2017-04-02 18:01:49 -0700

    Add F# Bob Solution

---
+++ fsharp/bob/Bob/Bob.fs
@@ -0,0 +1,8 @@
+﻿module Bob
+
+let hey (msg : string) =
+    if not(String.exists(fun c -> not(System.Char.IsWhiteSpace(c))) msg) then "Fine. Be that way!"
+    elif (String.exists(fun c -> System.Char.IsLetter(c)) msg)
+        && (msg.ToUpper()) = msg then "Whoa, chill out!"
+    elif msg.EndsWith("?") then "Sure."
+    else "Whatever."
