003cf0844c2cccb87c231fb7318a05ebd4418796
Emily Gorcenski <gorcee@hotmail.com>
2018-01-03 14:02:52 -0500

    Update word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,7 +3,6 @@ import string
 
 def word_count(phrase):
     trans = str.maketrans({'_': ' ', ',': ' ', '\n': ' ', '\t': ' ', ':': ''})
-    li = [w.strip(string.punctuation)
-          for w in phrase.translate(trans).lower().split()]
-    print(li)
-    return {w: li.count(w) for w in set(li)}
+    words = [w.strip(string.punctuation)
+             for w in phrase.translate(trans).lower().split()]
+    return {w: words.count(w) for w in set(words)}
