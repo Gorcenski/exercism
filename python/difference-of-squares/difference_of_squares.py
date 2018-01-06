89080b9e697e130cdfc06496599c9dd8b50d7b56
Emily Gorcenski <gorcee@hotmail.com>
2018-01-06 12:29:14 -0500

    add difference of squares

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,13 @@
+from math import pow
+
+
+def square_of_sum(n):
+    return pow(sum(range(1, n + 1)), 2)
+
+
+def sum_of_squares(n):
+    return sum([pow(x, 2) for x in range(1, n + 1)])
+
+
+def difference(n):
+    return square_of_sum(n) - sum_of_squares(n)
