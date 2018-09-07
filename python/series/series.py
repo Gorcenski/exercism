4d7473655175f28ecbfdf749b9af74a459f276d4
Emily Gorcenski <Gorcenski@users.noreply.github.com>
2018-09-07 17:45:31 +0200

    Improve series

--- python/series/series.py
+++ python/series/series.py
@@ -4,6 +4,7 @@ def slices(series, length):
     if length > len(series):
         raise ValueError("Length longer than input series")
 
-    substrings = [series[i:i + length] for i in range(len(series))]
-    return [list(map(lambda y: int(y), s)) for s in substrings
-            if len(s) == length]
+    substrings = [list(map(lambda y: int(y),
+                       series[i:i + length]))
+                  for i in range(len(series) - length + 1)]
+    return substrings
