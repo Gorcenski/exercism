11130d6ea6440ecff761452fe5ca0f524dac58a4
Emily Gorcenski <Gorcenski@users.noreply.github.com>
2019-06-23 19:57:04 +0200

    improve gigasecond

--- cpp/gigasecond/gigasecond.h
+++ cpp/gigasecond/gigasecond.h
@@ -5,10 +5,9 @@
 #include "boost/date_time/posix_time/posix_time.hpp" //no i/o just types
 
 namespace gigasecond {
-    const boost::posix_time::time_duration gigasecond = boost::posix_time::seconds(1000000000);
-    boost::posix_time::ptime advance(boost::posix_time::ptime start)
+    boost::posix_time::ptime advance(const boost::posix_time::ptime& start)
     {
-        return start + gigasecond;        
+        return start + boost::posix_time::seconds(1000000000);
     }
 }
 
