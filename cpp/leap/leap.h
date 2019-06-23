f18c4b1a64b624095969bf8bf601c5f9ee049d54
Emily Gorcenski <Gorcenski@users.noreply.github.com>
2019-06-23 11:01:23 +0200

    add c++ track and first two exercises

---
+++ cpp/leap/leap.h
@@ -0,0 +1,12 @@
+#define EXERCISM_RUN_ALL_TESTS
+#if !defined(LEAP_H)
+#define LEAP_H
+
+namespace leap {
+    bool is_leap_year(int year)
+    {
+        return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+}
+
+#endif
